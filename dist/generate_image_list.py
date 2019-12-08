d8fb804c65e087b18fa8f35260c1e028459b9770
glowinthedark <48893368+glowinthedark@users.noreply.github.com>
2019-12-08 20:33:39 +0100

    Update generate_image_list.py

--- dist/generate_image_list.py
+++ dist/generate_image_list.py
@@ -8,9 +8,16 @@ import glob
 assets = []
 
 glob_pat = sys.argv[1] if len(sys.argv) > 1 else '*.jpg'
-thumbs_dir = sys.argv[2] if len(sys.argv) > 2 else 'thumbs'
+thumbs_dir = sys.argv[2] if len(sys.argv) > 2 else 'thumbs/'
+download_url = sys.argv[3] if len(sys.argv) > 3 else '../'
 
 for f in sorted(glob.glob(glob_pat)):
-    assets.append({'src': f, 'thumb': thumbs_dir + f})
+    assets.append(
+        {
+            'src': f,
+            'thumb': thumbs_dir + f,
+            'downloadUrl': '../' + f
+        }
+    )
 
 print(json.dumps(assets, indent=1))
